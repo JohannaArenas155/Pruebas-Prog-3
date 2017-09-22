57ad5e880396bdf44a897722a1f45850a7ec8644
cristianc.bedoya <ccamilo.bedoya@gmail.com>
2017-09-21 20:13:45 -0500

    Borradas cosas innecesarias

--- routes/apiLOC.js
+++ routes/apiLOC.js
@@ -20,57 +20,6 @@ router.use(function(req, res, next) {
   next();
 });
 
-/*//Mostrar los resultados por ahora
-var xValue = [130,650,99,150,128,302,95,945,368,961];
-var yValue = [186,699,132,272,291,331,199,1890,788,1601];
-var numbersAmount = xValue.length;
-var xSummatory = 0;
-var ySummatory = 0;
-var xPow = [];
-var yPow = [];
-var xPowSummatory = 0;
-var yPowSummatory = 0;
-var bValue = 0;
-var aValue = 0;
-var xMultiplyY = [];
-var xMultiplyYSummatory = 0;
-var rValuePow = [];
-var rValue = 0;
-var xavg = 0;
-var yavg = 0;
-var yk = 0;
-
-for (var i = 0; i < numbersAmount; i++) {
-  xSummatory = xSummatory + xValue[i];
-  ySummatory = ySummatory + yValue[i];
-  xPow[i] = math.pow(xValue[i],2);
-  yPow[i] = math.pow(yValue[i],2);
-  xPowSummatory = xPowSummatory + xPow[i];
-  yPowSummatory = yPowSummatory + yPow[i];
-  xMultiplyY[i] = xValue[i]*yValue[i];
-  xMultiplyYSummatory = xMultiplyYSummatory + xMultiplyY[i];
-}
-
-xavg = xSummatory/numbersAmount;
-yavg = ySummatory/numbersAmount;
-
-bValue = (xMultiplyYSummatory - (xavg*yavg*numbersAmount))/(xPowSummatory - (math.pow(xavg,2)*numbersAmount));
-
-aValue = ((yavg) - (bValue*(xavg)));
-
-rValue = ((numbersAmount*xMultiplyYSummatory)-(xSummatory*ySummatory))/(math.sqrt(((numbersAmount*(xPowSummatory))-math.pow(xSummatory,2))*((numbersAmount*(yPowSummatory))-math.pow(ySummatory,2))));
-
-rValuePow = rValue*rValue;
-
-yk = aValue + bValue*386;
-
-console.log(aValue);
-console.log(bValue);
-console.log(rValue);
-console.log(rValuePow);
-console.log(yk);*/
-//Hasta aquí
-
 // Solo se necesita recibir archivos
 // Post para subir, con ruta /loc/api/upload
 router.post('/upload', function(req, res) {
